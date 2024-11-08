3737a5624f04120fa9967837e688514a8c9e8931
Cahú Victor <33499532+CahuVictor@users.noreply.github.com>
2024-11-07 21:53:45 -0300

    build: Colocando o restante dos arquivos que tinham sido removidos por engano

--- ESP32 S3/XEQUE-Mate_Projeto/src/main.cpp
+++ ESP32 S3/XEQUE-Mate_Projeto/src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
 
-#include "appl_MyLogger.h"
+//#include "appl_MyLogger.h"
+//#include "appl_LedControl.h"
+//#include "appl_RFIDControl.h"
+    //#include "ButtonControl.h"
+//#include "appl_LCDControl.h"
 #include "appl_SerialCommunication.h"
 #include "appl_WiFiManager.h"
+//#include "appl_WebServerControl.h"
+    /*#include "I2CCommunication.h"
+    #include "PowerMonitor.h"
+    #include "SupervisorTask.h"
+    #include "StateMachine.h"*/
 
 // Instâncias globais das classes
+//LedControl ledControl;
+//RFIDControl rfidControl;
+    //ButtonControl buttonControl;
+//LCDControl lcdControl;
 SerialCommunication serialComm(&Serial);  // Passa a interface Serial como parâmetro
+QueueHandle_t serialQueue;
 WiFiManager wifiManager;
 
 // Queue FreeRtos
@@ -44,6 +58,11 @@ void printQueueTask(void* pvParameters) {
         vTaskDelay(pdMS_TO_TICKS(10000));
     }
 }
+//WebServerControl webServer(8080);  // Porta 80 para o servidor web
+    /*I2CCommunication i2cComm;
+    PowerMonitor powerMonitor;
+    SupervisorTask supervisor;
+    StateMachine stateMachine;*/
 
 void setup() {
     // Inicialização da comunicação serial
@@ -74,23 +93,55 @@ void setup() {
     wifiManager.setQueue(queue, WiFiQueue);
 
     // Inicialização dos módulos
+    //ledControl.initialize();
+    //rfidControl.initialize();
+    //buttonControl.initialize();
+    //lcdControl.initialize();
     serialComm.initialize();
+    serialQueue = xQueueCreate(10, sizeof(char) * QUEUE_MESSAGE_SIZE);
+    wifiManager.setSerialQueue(serialQueue);    // Passa a fila para WiFiManager
     wifiManager.initialize();
 
     // Inicia as tarefas
     serialComm.startTask();
     wifiManager.startTask();
+    //webServer.initialize();
+        /*i2cComm.initialize();
+        powerMonitor.initialize();
+        supervisor.initialize();
+        stateMachine.initialize();*/
 
     // Inicia tarefas FreeRTOS para cada módulo (exemplo)
     //wifiManager.startTask();            //  http://http://192.168.4.1/
     //serialComm.setWiFiTaskHandle(wifiManager.getWiFiTaskHandle());
     //serialComm.startTask();
+    //ledControl.startTask();
+    //rfidControl.startTask();
+    /*xTaskCreate([](void*) { buttonControl.readButtons(); }, "Button Task", 2048, NULL, 1, NULL);
+    xTaskCreate([](void*) { powerMonitor.logConsumption(); }, "Power Monitor Task", 2048, NULL, 1, NULL);
+    xTaskCreate([](void*) { supervisor.monitorTasks(); }, "Supervisor Task", 2048, NULL, 1, NULL);*/
+    wifiManager.startWiFiTask();            //  http://http://192.168.4.1/
+    serialComm.setWiFiTaskHandle(wifiManager.getWiFiTaskHandle());
+    //webServer.startTask();                  //  http://<IP_DO_ESP>/     http://<IP_DO_ESP>/status
+    serialComm.startTasks();
+    //xTaskCreate([](void*) { stateMachine.updateState(0); }, "State Machine Task", 2048, NULL, 1, NULL);*
 
     // Configurações de teste
     
 
     // Cria a nova tarefa para monitorar a fila
     xTaskCreate(printQueueTask, "Queue Monitor Task", 2048, NULL, 1, NULL);
+    //ledControl.sendMessage("Iniciar teste de LEDs");
+    //rfidControl.sendMessageToRFIDQueue("Iniciar leitura RFID");
+    //rfidControl.setReadMode(CONTINUOUS_READ);  // Alterna para leitura contínua
+    //rfidControl.setReadInterval(3000);         // Define o intervalo de leitura contínua para 3 segundos
+    //rfidControl.setReadMode(READ_DISABLED);      // Configura para leitura única
+    //rfidControl.setReadMode(SINGLE_READ);      // Configura para leitura única
+    //rfidControl.sendMessageToRFIDQueue("Iniciar leitura única RFID");
+    //lcdControl.sendMessageToLCD(1, "Hello, LCD1!", 0);
+    //lcdControl.sendMessageToLCD(2, "Hello, LCD2!", 0);
+    serialComm.sendMessage("Hello from SerialCommunication!");
+    //webServer.sendMessageToWebServer("Mensagem inicial para o servidor!");
 }
 
 void loop()
