1bd3756184eaa3689b53a9639b66e086bfbc33e2
Selton Duarte <162406541+seltonjoa0@users.noreply.github.com>
2024-11-14 19:16:07 -0300

    refactor: Modularização do código do Player

---
+++ player/jogar.py
@@ -0,0 +1,46 @@
+import requests
+from realizar_jogada_engine import realizar_jogada_engine
+from realizar_jogada_aleatoria import realizar_jogada_aleatoria
+from realizar_jogada_usuario import realizar_jogada_usuario
+import chess
+
+tabuleiro_url = "http://localhost:5000/jogar"  # Endereço do Jogador 1
+
+def jogar(fen, board, jogador, controlar_brancas, controlar_pretas):
+    """Recebe a jogada do oponente, faz sua jogada e envia de volta."""
+    board.set_fen(fen)
+    print(f"\nJogador recebeu o tabuleiro FEN. Tabuleiro FEN: {fen}")
+    print(board)
+    
+    if not board.is_game_over():
+        if jogador == 1:
+            print(controlar_brancas)
+            if controlar_brancas == 1:
+                jogada = realizar_jogada_engine(board.fen())
+            elif controlar_brancas == 2:
+                jogada = realizar_jogada_aleatoria(board.fen())
+            elif controlar_brancas == 3: 
+                jogada = realizar_jogada_usuario(board.fen())
+            board.push(chess.Move.from_uci(jogada))
+            jogador = 2
+        elif jogador == 2:
+            if controlar_pretas == 1:
+                jogada = realizar_jogada_engine(board.fen())
+            elif controlar_pretas == 2:
+                jogada = realizar_jogada_aleatoria(board.fen())
+            elif contolar_pretas == 3: 
+                jogada = realizar_jogada_usuario(board.fen())
+            board.push(chess.Move.from_uci(jogada))
+            jogador = 1
+        
+        # Enviar a jogada para o jogador 1
+        try:
+            response = requests.post(tabuleiro_url, json={'fen': board.fen()}, timeout=5)
+            if response.status_code == 200:
+                print("\nJogada enviada para o tabuleiro.")
+        except requests.RequestException as e:
+            print(f"Erro: {e}")
+    else:
+        print("O jogo acabou.")
+    
+    return board, jogador  # Retorna o novo estado do tabuleiro e o jogador atualizado
